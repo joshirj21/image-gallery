700172722d54c57aa4cc616f79e6e226f052670b
Rahul joshi <joshi.rj21@gmail.com>
2019-08-28 19:42:05 +0530

    Added animating toggler

--- navbar/navbar-4/script.js
+++ navbar/navbar-4/script.js
@@ -7,3 +7,16 @@ search.addEventListener("click", function () {
 document.querySelector(".fa-times").parentNode.addEventListener("click", function () {
     document.querySelector(".search").style.display = "none";
 })
+
+var navbartoggler = document.querySelector(".navbar-toggler");
+navbartoggler.addEventListener("click", function () {
+    var toggler = document.querySelector(".toggler")
+    toggler.childNodes[1].classList.toggle("isactive")
+    toggler.childNodes[3].classList.toggle("isactive")
+    toggler.childNodes[5].classList.toggle("isactive")
+
+    // anime({
+    //     targets: this.childNodes[1],
+    //     rotateX: "360deg" // -> '540deg'
+    // });
+})
